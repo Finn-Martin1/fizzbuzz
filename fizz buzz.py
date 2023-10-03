dfe6db173155c16a190ebeeec7425e409b0940c8
Finn Martin <19martin.f@stbenedicts.essex.sch.uk>
2023-10-03 11:15:33 +0000

    Pending changes exported from your codespace

---
+++ fizz buzz.py
@@ -0,0 +1,14 @@
+#fizz buzz game in python code
+
+for i in range(1,16):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif i % 3 == 0:
+        print("fizz")
+        continue
+    elif i % 5 == 0:
+        print("buzz")
+        continue
+    print(i)
+
